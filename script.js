63c2c9dbe518069f45ea3e63d5ca31e4abf7d828
Michael Plantamura <105463635+Michaelplant2@users.noreply.github.com>
2023-06-01 16:25:14 -0400

    reworked api

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
+let SWSearchUrl = "https://swapi.dev/api/people/?search=";
 let wikiUrl = "https://en.wikipedia.org/api/rest_v1/page/summary/";
-let starWarsUrl = "https://swapi.dev/api/";
+
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+const name = document.getElementById("name-info");
+const bio = document.getElementById("person-info");
+
+getJSON(SWSearchUrl);
+
+async function getJSON(url) {
+    const response = await fetch(url);
+    const responseData = await response.json();
+
+    name.innerHTML = `
+        <h1>${name}</h1>
+    `;
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const searchTerm = search.value;
+    if (searchTerm) {
+        getJSON(SWSearchUrl + searchTerm);
+        search.value = "";
+    }
+});
