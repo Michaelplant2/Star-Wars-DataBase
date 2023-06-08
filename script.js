a61e001cc96032b7a62f9627c233e8611d6e9658
Michael Plantamura <105463635+Michaelplant2@users.noreply.github.com>
2023-06-08 15:26:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let SWSearchUrl = "https://swapi.dev/api/people/?search=";
+let SwUrl = "https://swapi.dev/api/people/?search=";
 let wikiUrl = "https://en.wikipedia.org/api/rest_v1/page/summary/";
 
 const form = document.getElementById("form");
@@ -6,36 +6,40 @@ const search = document.getElementById("search");
 const name = document.getElementById("name-info");
 const bio = document.getElementById("person-info");
 
-getJSON(SWSearchUrl);
-
-async function getJSON(url) {
+async function getSwInfo(url) {
+  try {
     const response = await fetch(url);
-    const responseData = await response.json();
-
-    name.innerHTML = `
-        <h1>${name}</h1>
-    `;
+    const data = await response.json();
+    listData(data);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-    // bio.innerHTML = `
-    // <ul>
-    // <li>Home Planet:</li>
-    // <li>Birth Year:</li>
-    // <li>Gender:</li>
-    // <li>Species:</li>
-    // <li>Height:</li>
-    // <li>Weight:</li>
-    // <li>Hair Color:</li>
-    // <li>Eye Color:</li>
-    // <li>Films:</li>
-    // </ul>
-    // `
+getSWInfo(SwUrl);
+
+function listData(data) {
+  data[1].map(function (country) {
+    const countryName = `
+          <li id="country">${country.name}</li>
+        `;
+  });
+}
+
+// bio.innerHTML = `
+// <ul>
+// <li>Home Planet:</li>
+// <li>Birth Year:</li>
+// <li>Gender:</li>
+// <li>Species:</li>
+// <li>Height:</li>
+// <li>Weight:</li>
+// <li>Hair Color:</li>
+// <li>Eye Color:</li>
+// <li>Films:</li>
+// </ul>
+// `
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const searchTerm = search.value;
-    if (searchTerm) {
-        getJSON(SWSearchUrl + searchTerm);
-        search.value = "";
-    }
+  e.preventDefault();
 });
