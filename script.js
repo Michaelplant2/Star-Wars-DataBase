95c7016221025d3e8cca7a69693129c68c032356
Michael Plantamura <105463635+Michaelplant2@users.noreply.github.com>
2023-06-08 16:39:15 -0400

    connected api

--- script.js
+++ script.js
@@ -1,45 +1,55 @@
-let SwUrl = "https://swapi.dev/api/people/?search=";
-let wikiUrl = "https://en.wikipedia.org/api/rest_v1/page/summary/";
+let swUrl = "https://swapi.dev/api/people/?search=";
+let imgUrl = "https://en.wikipedia.org/api/rest_v1/page/summary/";
 
 const form = document.getElementById("form");
 const search = document.getElementById("search");
-const name = document.getElementById("name-info");
-const bio = document.getElementById("person-info");
+const nameInfo = document.getElementById("name-info");
+const bioInfo = document.getElementById("person-info");
 
 async function getSwInfo(url) {
   try {
     const response = await fetch(url);
     const data = await response.json();
-    listData(data);
+    nameData(data.results);
+    bioData(data.results)
   } catch (error) {
     console.log(error);
   }
 }
+getSwInfo(swUrl);
 
-getSWInfo(SwUrl);
-
-function listData(data) {
-  data[1].map(function (country) {
-    const countryName = `
-          <li id="country">${country.name}</li>
-        `;
+function nameData(persons) {
+  persons.forEach((person) => {
+    const {name} = person;
+    nameInfo.innerHTML = `
+        <h1>${name}</h1>
+        <img src="${imgUrl + name}"/>`;
   });
 }
 
-// bio.innerHTML = `
-// <ul>
-// <li>Home Planet:</li>
-// <li>Birth Year:</li>
-// <li>Gender:</li>
-// <li>Species:</li>
-// <li>Height:</li>
-// <li>Weight:</li>
-// <li>Hair Color:</li>
-// <li>Eye Color:</li>
-// <li>Films:</li>
-// </ul>
-// `
+function bioData(bios) {
+    bios.forEach((bio) => {
+      const {homeworld, birth_year, gender, species, height, mass, hair_color, eye_color, films} = bio;
+      bioInfo.innerHTML = `
+        <ul>
+            <li>Home Planet: ${homeworld}</li>
+            <li>Birth Year: ${birth_year}</li>
+            <li>Gender: ${gender}</li>
+            <li>Species: ${species}</li>
+            <li>Height: ${height}</li>
+            <li>Weight: ${mass}</li>
+            <li>Hair Color: ${hair_color}</li>
+            <li>Eye Color: ${eye_color}</li>
+            <li>Films: ${films}</li>
+        </ul>`;
+    });
+  }
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  const searchTerm = search.value;
+  if (searchTerm) {
+    getSwInfo(swUrl + searchTerm);
+    search.value = "";
+  }
 });
