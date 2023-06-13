1b028002fb4fbd4e28cf18c1581852ab7b2228cd
Michael Plantamura <105463635+Michaelplant2@users.noreply.github.com>
2023-06-13 16:17:46 -0400

    added css

--- script.js
+++ script.js
@@ -1,49 +1,10 @@
 let swUrl = "https://swapi.dev/api/people/?search=";
-let imgUrl = "https://en.wikipedia.org/api/rest_v1/page/summary/";
+// let imgUrl = "https://en.wikipedia.org/api/rest_v1/page/summary/";
 
 const form = document.getElementById("form");
 const search = document.getElementById("search");
 const nameInfo = document.getElementById("name-info");
 const bioInfo = document.getElementById("person-info");
-
-async function getSwInfo(url) {
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-    nameData(data.results);
-    bioData(data.results)
-  } catch (error) {
-    console.log(error);
-  }
-}
-getSwInfo(swUrl);
-
-function nameData(persons) {
-  persons.forEach((person) => {
-    const {name} = person;
-    nameInfo.innerHTML = `
-        <h2 id="name">${name}</h2>
-        <img src="${imgUrl + name}"/>`;
-  });
-}
-
-function bioData(bios) {
-    bios.forEach((bio) => {
-      const {homeworld, birth_year, gender, species, height, mass, hair_color, eye_color, films} = bio;
-      bioInfo.innerHTML = `
-        <ul>
-            <li><h3>Home Planet:</h3> ${homeworld}</li>
-            <li><h3>Birth Year:</h3> ${birth_year}</li>
-            <li><h3>Gender:</h3> ${gender}</li>
-            <li><h3>Species:</h3> ${species}</li>
-            <li><h3>Height:</h3> ${height}</li>
-            <li><h3>Weight:</h3> ${mass}</li>
-            <li><h3>Hair Color:</h3> ${hair_color}</li>
-            <li><h3>Eye Color:</h3> ${eye_color}</li>
-            <li><h3>Films:</h3> ${films}</li>
-        </ul>`;
-    });
-  }
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -53,3 +14,49 @@ form.addEventListener("submit", (e) => {
     search.value = "";
   }
 });
+
+async function getSwInfo(url) {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    nameData(data.results);
+    bioData(data.results);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function nameData(persons) {
+  persons.forEach((person) => {
+    const { name } = person;
+    nameInfo.innerHTML = `
+        <h2 id="name">${name}</h2>
+        <p class="aurebesh">${name}</p>`;
+  });
+}
+
+function bioData(bios) {
+  bios.forEach((bio) => {
+    const {
+      homeworld,
+      birth_year,
+      gender,
+      species,
+      height,
+      mass,
+      hair_color,
+      eye_color,
+    } = bio;
+    bioInfo.innerHTML = `
+        <ul>
+            <li><h3>Home Planet:</h3> ${homeworld}</li>
+            <li><h3>Birth Year:</h3> ${birth_year}</li>
+            <li><h3>Gender:</h3> ${gender}</li>
+            <li><h3>Species:</h3> ${species}</li>
+            <li><h3>Height:</h3> ${height}</li>
+            <li><h3>Weight:</h3> ${mass}</li>
+            <li><h3>Hair Color:</h3> ${hair_color}</li>
+            <li><h3>Eye Color:</h3> ${eye_color}</li>
+        </ul>`;
+  });
+}
