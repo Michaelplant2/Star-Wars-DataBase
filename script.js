6611357e39a31106e560efe695070aef561ea9b0
Michael Plantamura <105463635+Michaelplant2@users.noreply.github.com>
2023-06-27 16:53:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ async function getSwInfo(url) {
   } catch (error) {
     console.log(error);
   }
-}
+};
 
 async function getHome(person) {
   const response = await fetch(person.homeworld);
@@ -37,7 +37,7 @@ async function getSpecies(person) {
   const speciesData = await response.json();
   person.species = speciesData.name;
   return person;
-}
+};
 
 function personData(data) {
   const person = data[0];
@@ -57,58 +57,4 @@ function personData(data) {
       <li><h3>Eye Color: </h3><p>${person.eye_color}</p></li>
     </ul>`;
   personInfo.innerHTML = personBio;
-}
-
-// async function getSwInfo(url) {
-//   try {
-//     const response = await fetch(url);
-//     const data = await response.json();
-//     nameData(data.results);
-//     bioData(data.results);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-
-// function nameData(persons) {
-//   persons.forEach((person) => {
-//     const { name } = person;
-//     nameInfo.innerHTML = `
-//         <h2 id="name">${name}</h2>
-//         <p class="aurebesh">${name}</p>`;
-//   });
-// }
-
-// async function callHome(bio) {
-//   const response = await fetch(bio.homeworld);
-//   const data = await response.json();
-//   bio.homeworld = data.name;
-//   return bio
-// }
-
-// function bioData(bios) {
-//   bios.forEach(async(bio) => {
-//     bio = await callHome(bio);
-//     const {
-//       homeworld,
-//       birth_year,
-//       gender,
-//       species,
-//       height,
-//       mass,
-//       hair_color,
-//       eye_color,
-//     } = bio;
-//     personInfo.innerHTML = `
-//         <ul>
-//             <li><h3>Home Planet: </h3><p>${homeworld}</p></li>
-//             <li><h3>Birth Year: </h3><p>${birth_year}</p></li>
-//             <li><h3>Gender: </h3><p>${gender}</p></li>
-//             <li><h3>Species: </h3><p>${species}</p></li>
-//             <li><h3>Height: </h3><p>${height}</p></li>
-//             <li><h3>Weight: </h3><p>${mass}</p></li>
-//             <li><h3>Hair Color: </h3><p>${hair_color}</p></li>
-//             <li><h3>Eye Color: </h3><p>${eye_color}</p></li>
-//         </ul>`;
-//   });
-// }
+};
