c0479e101c502e709c048cf496cd6b85dd36dd73
Michael Plantamura <105463635+Michaelplant2@users.noreply.github.com>
2023-06-27 16:51:20 -0400

    adding homeworld and species

--- script.js
+++ script.js
@@ -23,14 +23,21 @@ async function getSwInfo(url) {
   } catch (error) {
     console.log(error);
   }
+}
+
+async function getHome(person) {
+  const response = await fetch(person.homeworld);
+  const homeData = await response.json();
+  person.homeworld = homeData.name;
+  return person;
 };
 
-// async function callHome(data) {
-//   const response = await fetch(data.homeworld);
-//   const data = await response.json();
-//   data.homeworld = data.name;
-//   return data;
-// };
+async function getSpecies(person) {
+  const response = await fetch(person.species);
+  const speciesData = await response.json();
+  person.species = speciesData.name;
+  return person;
+}
 
 function personData(data) {
   const person = data[0];
@@ -40,18 +47,17 @@ function personData(data) {
   nameInfo.innerHTML = personName;
   let personBio = `
     <ul>
-      <li><h3>Home Planet: </h3><p>${person.homeworld}</p></li>
+      <li><h3>Home Planet: </h3><p>${getHome(person)}</p></li>
       <li><h3>Birth Year: </h3><p>${person.birth_year}</p></li>
       <li><h3>Gender: </h3><p>${person.gender}</p></li>
-      <li><h3>Species: </h3><p>${person.species}</p></li>
+      <li><h3>Species: </h3><p>${getSpecies(person)}</p></li>
       <li><h3>Height: </h3><p>${person.height}</p></li>            
       <li><h3>Weight: </h3><p>${person.mass}</p></li>
       <li><h3>Hair Color: </h3><p>${person.hair_color}</p></li>
       <li><h3>Eye Color: </h3><p>${person.eye_color}</p></li>
     </ul>`;
   personInfo.innerHTML = personBio;
-};
-
+}
 
 // async function getSwInfo(url) {
 //   try {
