8ea81e6efe403f86d818205cb88cab50e90fff48
Michael Plantamura <105463635+Michaelplant2@users.noreply.github.com>
2023-07-14 11:03:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ async function getSwInfo(url) {
   console.log(data);
   personData(data.results);
   if (data.count == 0) { 
-    alert("Error finding information.\nTry a new name or variation");
+    alert("\t\t\tError finding information.\n\t\t\tTry a new name or variation");
   }
 };
 
